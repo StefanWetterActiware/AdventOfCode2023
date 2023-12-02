787e3eb7ae09267588ef43a2277a932068a26c4f
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2023-12-02 08:08:07 +0100

    Day1b - falsches Ergebnis

--- Program.cs
+++ Program.cs
@@ -1,16 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
 Console.WriteLine("Hello, World!");
 
 var lines = System.IO.File.ReadAllLines("input/day1");
-var sum = 0;
-System.Text.RegularExpressions.Regex r = new(@"\d");
+System.Text.RegularExpressions.Regex r = new(@"(\d|one|two|three|four|five|six|seven|eight|nine)");
 
+var num2num = (Match a) => {
+    switch (a.Value)
+    {
+        case "one": return 1;
+        case "two": return 2;
+        case "three": return 3;
+        case "four": return 4;
+        case "five": return 5;
+        case "six": return 6;
+        case "seven": return 7;
+        case "eight": return 8;
+        case "nine": return 9;
+        default:
+        return int.Parse(a.Value);
+    }
+};
+
+var sum = 0;
 foreach (string line in lines)
 {
-    var first = r.Match(line);
-    var last = r.Match(String.Join(' ', line.Reverse()));
+    var matches = r.Matches(line);
+    var first = matches.First();
+    var last = matches.Last();
 
-    var no = int.Parse(first.Value + last.Value);
+    var no = int.Parse($"{num2num(first)}{num2num(last)}");
+    Console.WriteLine(no);
     sum += no;
 }
 

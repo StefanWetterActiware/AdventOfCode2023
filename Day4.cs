e327e2a92bbfbbe4a5141b97419a26c0c428c516
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2023-12-04 07:54:16 +0100

    d4p2

--- Day4.cs
+++ Day4.cs
@@ -10,8 +10,17 @@ static class Day4{
         double sumA = 0;
         var sumB = 0;
 
+        var n=0;
+        Dictionary<int,int> howMany = new();
         foreach (var line in lines)
         {
+            howMany.Add(++n, 1);
+        }
+
+        var no = 0;
+        foreach (var line in lines)
+        {
+            no++;
             var parts = line.Split(":");
             var nos = parts[1].Split("|");
             var winners = nos[0].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
@@ -19,13 +28,19 @@ static class Day4{
             var anz = mine.Where(a=>winners.Contains(a)).Count();
 
             if (anz>0){
-            var val = Math.Pow(2,anz-1);
-            sumA+=val;
+                var val = Math.Pow(2,anz-1);
+                sumA+=val;
+
+                for (int i = no+1; i <= no+anz; i++)
+                {
+                    if (howMany.ContainsKey(i))
+                    howMany[i]+=1*howMany[no];
+                }
             }
         }
 
 
         Console.WriteLine($"Sum: {sumA}");
-        Console.WriteLine($"SumB: {sumB}");
+        Console.WriteLine($"SumB: {howMany.Values.Sum()}");
     }
 }

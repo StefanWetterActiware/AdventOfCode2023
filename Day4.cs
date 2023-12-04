3efc71ddd3374ea6123387884af4bc91a3304910
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2023-12-04 06:12:31 +0100

    4part1

--- Day4.cs
+++ Day4.cs
@@ -4,10 +4,25 @@ using System.Text.RegularExpressions;
 static class Day4{
     internal static void doit(){
         Regex dayNoR = new(@"\d*$");
-        var lines = Helper.getInputAsLines(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value));
+        var lines = Helper.getInputAsLines(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value),
+                                           test:false);
         
-        var sumA = 0;
+        double sumA = 0;
         var sumB = 0;
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(":");
+            var nos = parts[1].Split("|");
+            var winners = nos[0].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var mine = nos[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var anz = mine.Where(a=>winners.Contains(a)).Count();
+
+            if (anz>0){
+            var val = Math.Pow(2,anz-1);
+            sumA+=val;
+            }
+        }
 
 
         Console.WriteLine($"Sum: {sumA}");

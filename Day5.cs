261b9bb3d73b7ce6b15ed5ba5380591fc1b601ea
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2023-12-05 13:12:14 +0100

    5part2 - BAD

--- Day5.cs
+++ Day5.cs
@@ -9,28 +9,43 @@ static class Day5{
                                            test:false);
         lines.Append("");
 
-        var sumA = 0;
-        var sumB = 0;
+        var valB = long.MaxValue;
 
         var startseeds = lines.First().Split(':')[1].Trim().Split(' ',StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(a => long.Parse(a)).ToList();
         var levelvals = new List<List<long>>();
         levelvals.Add(startseeds);
 
+        var allTabs = new List<List<List<long>>>();
+
+        var oneStep = (List<List<long>> tab, long no) => {
+            var f = tab.Where(a=> no >= a[1] && no < a[1]+a[2]);
+            if (f.Any()) {
+                var a = f.First();
+                return no - (a[1]-a[0]);
+            } else {
+                return no;
+            }
+        };
+
         var step = (List<List<long>> tab) => {
             if (tab.Any()) {
+                allTabs.Add(tab);
+
                 var newLine = new List<long>();
                 foreach (var source in levelvals.Last())
                 {
-                    var f = tab.Where(a=> source >= a[1] && source < a[1]+a[2]);
-                    if (f.Any()) {
-                        var a = f.First();
-                        newLine.Add(source - (a[1]-a[0]));
-                    } else {
-                        newLine.Add(source);
-                    }
+                    newLine.Add(oneStep(tab, source));
                 }
                 levelvals.Add(newLine);
             }
+        };
+
+        var allSteps = (long no) => {
+            foreach (var lev in allTabs)
+            {
+                no=oneStep(lev, no);
+            }
+            return no;
         };
 
         var tab = new List<List<long>>();
@@ -39,7 +54,7 @@ static class Day5{
             if (!String.IsNullOrWhiteSpace(line)){
                 if (line.EndsWith("ap:")) {
                     step(tab);
-                    tab.Clear();
+                    tab=new();
                 } else {
                     var l = line.Split(' ', StringSplitOptions.TrimEntries|StringSplitOptions.RemoveEmptyEntries);
                     var d = long.Parse(l[0]);
@@ -51,8 +66,18 @@ static class Day5{
         }
         step(tab);
 
+        //Völlige Gurkenlösung. Brute Force. Nach 2/3 auf gut Glück den aktuellen Min-Wert eingegeben und passt. Glück gehabt
+        // Besser wäre vermutlich einfach, bei jeder Berechnung zurückzubekommen,
+        // wie viele Zahlen ab der zuletzt berechneten in allen Stufen noch ins gleiche Raster fallen, und die dann einfach zu überspringen
+        for (int i = 0; i < startseeds.Count; i+=2)
+        {
+            for (long j = startseeds[i]; j < startseeds[i] + startseeds[i+1]; j++)
+            {
+                valB=Math.Min(valB, allSteps(j));
+            }
+        }
 
-        Console.WriteLine($"Lowest: {levelvals.Last().Min()}");
-        Console.WriteLine($"SumB: {sumB}");
+        Console.WriteLine($"LowestA: {levelvals.Last().Min()}");
+        Console.WriteLine($"LowestB: {valB}");
     }
 }

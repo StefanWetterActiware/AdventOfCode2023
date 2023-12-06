574fc248e7274d066d2fb0fa6f89d38a1c9136db
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2023-12-06 06:20:07 +0100

    day6

--- Day6.cs
+++ Day6.cs
@@ -6,13 +6,37 @@ static class Day6{
         Regex dayNoR = new(@"\d*$");
 
         var lines = Helper.getInputAsLines(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value),
-                                           test:true);
+                                           test:false);
         
         
-        var sumA = 0;
-        var sumB = 0;
+        long sumA = 1;
+        long sumB = 0;
 
+        var times = lines.First().Split(":")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(a=>long.Parse(a));
+        var records = lines.Last().Split(":")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(a=>long.Parse(a));
 
+        for (int i = 0; i < times.Count(); i++)
+        {
+            var time=times.Skip(i).First();
+            var rec=records.Skip(i).First();
+
+            long count=0;
+            for (long j = 0; j <= time; j++)
+            {
+                var meters = (time-j)*j;
+                if (meters>rec) count++;
+            }
+            sumA*=count;
+        }
+        
+        var time2 = long.Parse(lines.First().Split(":")[1].Replace(" ",""));
+        var rec2 = long.Parse(lines.Last().Split(":")[1].Replace(" ",""));
+        
+        for (long j = 0; j <= time2; j++)
+        {
+            var meters = (time2-j)*j;
+            if (meters>rec2) sumB++;
+        }
 
         Console.WriteLine($"Sum: {sumA}");
         Console.WriteLine($"SumB: {sumB}");

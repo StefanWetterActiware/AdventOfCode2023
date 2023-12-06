3375d3dabe4f9eeea82a565236c5407d55c98101
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2023-12-06 06:20:45 +0100

    prepare Day7

---
+++ Day7.cs
@@ -0,0 +1,20 @@
+using System.Text.RegularExpressions;
+
+
+static class Day7{
+    internal static void doit(){
+        Regex dayNoR = new(@"\d*$");
+
+        var lines = Helper.getInputAsLines(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value),
+                                           test:true);
+        
+        
+        var sumA = 0;
+        var sumB = 0;
+
+
+
+        Console.WriteLine($"Sum: {sumA}");
+        Console.WriteLine($"SumB: {sumB}");
+    }
+}

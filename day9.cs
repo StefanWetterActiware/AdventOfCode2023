f5e3db0e176cea7ec68e13ac115396fd8ec50ce2
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2023-12-09 12:15:10 +0100

    d9

--- day9.cs
+++ day9.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 
@@ -6,13 +7,39 @@ static class Day9 {
         Regex dayNoR = new(@"\d*$");
 
         var lines = Helper.getInputAsLines(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value),
-                                           test:true);
+                                           test:false);
 
 
-        var sumA = 0;
-        var sumB = 0;
+        var sumA = 0L;
+        var sumB = 0L;
 
+        foreach (var line in lines)
+        {
+            List<long[]> all=new();
+            all.Add(line.Split(" ",StringSplitOptions.RemoveEmptyEntries|StringSplitOptions.RemoveEmptyEntries).Select(a=>long.Parse(a)).ToArray());
+            var offset = all.First().First();
 
+            while(!all.Last().All(a=>a==0)){
+                var nextL = new List<long>();
+                for (int i = 0; i < all.Last().Count()-1; i++)
+                {
+                    nextL.Add(all.Last()[i+1] - all.Last()[i]);
+                }
+                all.Add(nextL.ToArray());
+            }
+            
+            all.Reverse();
+
+            long last=all.Skip(1).First().First();
+            long prev=last;
+            for (int i = 2; i < all.Count; i++)
+            {
+                last = all[i].Last() + last;
+                prev = all[i].First() - prev;
+            }
+            sumA+=last;
+            sumB+=prev;
+        }
 
         Console.WriteLine($"Sum: {sumA}");
         Console.WriteLine($"SumB: {sumB}");

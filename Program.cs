ab2fb407877ca276ef82e29c3f78a44e2e681a60
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2023-12-10 13:53:01 +0100

    fix finding correct class for day

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-var lastDayT = System.Reflection.Assembly.GetExecutingAssembly().GetTypes().Where(a => a.Name.StartsWith("Day")).OrderByDescending(a => a.Name).First();
+var lastDayT = System.Reflection.Assembly.GetExecutingAssembly().GetTypes().Where(a => a.Name.StartsWith("Day")).OrderByDescending(a => int.Parse(a.Name.Replace("Day",""))).First();
 //var lastDay = System.Activator.CreateInstance(lastDayT);
 
 var doitM = lastDayT.GetMethod("doit", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);

064406b1e519f52e7cc34dfeb073b1f44b9380aa
StefanWe <31532767+StefanWetterActiware@users.noreply.github.com>
2023-12-12 18:35:06 +0100

    Almost BruteForce - works4p1

--- Day12.cs
+++ Day12.cs
@@ -1,5 +1,7 @@
 using System.Drawing;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Text.RegularExpressions;
 
 static class Day12 {
@@ -8,24 +10,37 @@ static class Day12 {
 
         var input = Helper.getInput(int.Parse(dayNoR.Match(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name).Value));
                                            
-        input = """
-???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1
-""";
+//         input = """
+// ???.### 1,1,3
+// .??..??...?##. 1,1,3
+// ?#?#?#?#?#?#?#? 1,3,1,6
+// ????.#...#... 4,1,1
+// ????.######..#####. 1,6,5
+// ?###???????? 3,2,1
+// """;
 
-        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToDictionary(a=> a.Split(' ')[0], a=> a.Split(' ')[1].Split(',').Select(a=>int.Parse(a)).ToList());
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
 
         var sumA = 0;
         var sumB = 0;
 
-        foreach (var kvp in lines)
+        Regex amAnfangPassend = new("^[\\?#]*$");
+
+        var before=0;
+        System.IO.File.Delete("counterNoBF.txt");
+        foreach (var lineRaw in lines)
         {
-            var groups=kvp.Value;
-            var line=kvp.Key.Trim('.');
+            System.IO.File.AppendAllText("counterNoBF.txt", $"{sumA-before}{Environment.NewLine}");
+            before=sumA;
+
+            var rawParts = lineRaw.Split(' ');
+            //Teil2
+            rawParts[0] = rawParts[0] + '?' + rawParts[0] + '?' + rawParts[0] + '?' + rawParts[0] + '?' + rawParts[0];
+            rawParts[1] = rawParts[1] + ',' + rawParts[1] + ',' + rawParts[1] + ',' + rawParts[1] + ',' + rawParts[1];
+
+            //.Select(a=> new KeyValuePair(a.Split(' ')[0], )
+            var groups=rawParts[1].Split(',').Select(a=>int.Parse(a)).ToList();
+            var line=rawParts[0].Trim('.');
             var l = line.Length;
             var minL = groups.Sum()+groups.Count()-1;
             if (l==minL){
@@ -64,25 +79,36 @@ static class Day12 {
                     somethingfound=true;
                 }
 
-                else if (line.Substring(groups.First(),1) == "#"){
-                    // Wenn EINS weiter hinen als die Länge der ersten Gruppe ein # ist, MUSS das erste ein Leerding sein, damit wie oben
-                    line=line.Substring(groups.First()+1);
-                    groups=groups.Skip(1).ToList();
-                    if (line.StartsWith('?')){
-                        line=line.Substring(1);
-                    }
-                    line=line.Trim('.');
-                    somethingfound=true;
-                }
-                else if (line.Substring(line.Length-1-groups.Last(),1) == "#"){
-                    line=line.Substring(0,line.Length-groups.Last()-1);
-                    groups=groups.Take(groups.Count()-1).ToList();
-                    if (line.EndsWith('?')){
-                        line=line.Substring(0,line.Length-1);
-                    }
-                    line=line.Trim('.');
-                    somethingfound=true;
-                }
+                // else if (line.Substring(groups.First(),1) == "#"){
+                //     // Wenn EINS weiter hinen als die Länge der ersten Gruppe ein # ist, MUSS das erste ein Leerding sein, damit wie oben
+                //     line=line.Substring(groups.First()+1);
+                //     groups=groups.Skip(1).ToList();
+                //     if (line.StartsWith('?')){
+                //         line=line.Substring(1);
+                //     }
+                //     line=line.Trim('.');
+                //     somethingfound=true;
+                // }
+
+                // else if (line.Substring(line.Length-groups.Last(),1) == "#"){
+                //     line=line.Substring(0,line.Length-groups.Last()-1);
+                //     groups=groups.Take(groups.Count()-1).ToList();
+                //     if (line.EndsWith('?')){
+                //         line=line.Substring(0,line.Length-1);
+                //     }
+                //     line=line.Trim('.');
+                //     somethingfound=true;
+                // }
+
+                // else if (line.Substring(groups.First(),1).Equals(".") && amAnfangPassend.IsMatch(line.Substring(0,groups.First()))){
+                //     line=line.Substring(groups.First());
+                //     groups=groups.Skip(1).ToList();
+                //     if (line.StartsWith('?')){
+                //         line=line.Substring(1);
+                //     }
+                //     line=line.Trim('.');
+                //     somethingfound=true;
+                // }
             }
 
             if (groups.Count() == 0){
@@ -94,33 +120,64 @@ static class Day12 {
             line=line.Trim('.');
 
 
-            System.Text.RegularExpressions.Regex fr = new("\\?");
-            var frM = fr.Matches(line);
-            var anzFragez=frM.Count;
-
             // Wenn die Anzahl der GRUPPEN von Fragezeichen jetzt gleich der Anzahl in den "groups" ist, dann...
             System.Text.RegularExpressions.Regex frGrp = new("\\?+");
             var frGrpM = frGrp.Matches(line);
-            if (frGrpM.Count == groups.Count()){
-                var zwischenres=1;
-                for (int i = 0; i < groups.Count(); i++)
-                {
-                    Match match = frGrpM[i];
-                    zwischenres*=(match.Length-groups[i]+1);
-                }
-                sumA+=zwischenres;
-                continue;
-            }
+            // if (frGrpM.Count == groups.Count()){
+            //     var zwischenres=1;
+            //     for (int i = 0; i < groups.Count(); i++)
+            //     {
+            //         Match match = frGrpM[i];
+            //         zwischenres*=(match.Length-groups[i]+1);
+            //     }
+            //     sumA+=zwischenres;
+            //     continue;
+            // }
             
 
-            // TODO: 6 Fragezeichen und 2,1 zum Beispiel. Oder meherere Gruppen von fragezeichen mit mehreren Gruppen von Zahlen
-            line=line.Replace("?",".");
-            //Tolle Schleife über ALLLLLE Möglichkeiten?
+            if (frGrpM.Count == 1) {
+                var minZeich = groups.Count-1 + groups.Sum();
+                var luft = frGrpM[0].Length-minZeich;
+                var anzahlPlatzierungen = luft+groups.Count;
+                var oben = 1;
+                for (int i = 1; i <= anzahlPlatzierungen; i++)
+                {
+                    oben*=i;
+                }
+                var unten=1;
+                for (int i = 1; i <= anzahlPlatzierungen-luft; i++)
+                {
+                    unten*=i;
+                }
+                for (int i = 1; i <= luft; i++)
+                {
+                    unten*=i;
+                }
 
-            var countGrups = line.Split('.',StringSplitOptions.RemoveEmptyEntries);
-            if (countGrups.Length == groups.Count() && String.Join(",",countGrups.Select(a=>a.ToString())).Equals(String.Join(",",countGrups.Select(a=>a.ToString())))){
-                sumA++;
-            }
+                sumA+=oben/unten;
+            } else {
+                System.Text.RegularExpressions.Regex fr = new("\\?");
+                var frM = fr.Matches(line);
+                var anzFragez=frM.Count;
+                for (int i = 0; i < Math.Pow(2,anzFragez); i++)
+                {
+                    var bruteForceSrcLine=line.ToCharArray();
+                    for (int j = 0; j < anzFragez; j++)
+                    {
+                        if ((i & (int)Math.Pow(2,j)) == (int)Math.Pow(2,j)) {
+                            bruteForceSrcLine[frM[j].Index] = '#';
+                        } else {
+                            bruteForceSrcLine[frM[j].Index] = '.';
+                        }
+                    }
+
+                    var countGrups = new string(bruteForceSrcLine).Split('.',StringSplitOptions.RemoveEmptyEntries);
+                    if (countGrups.Length == groups.Count() && String.Join(",",groups.Select(a=>a.ToString())).Equals(String.Join(",",countGrups.Select(a=>a.ToString().Length)))){
+                        sumA++;
+                    }
+                }
+                Console.WriteLine($"{sumA}");
+             }
         }
 
 
